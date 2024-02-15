2ba5da2a73ba1df9a2ec8db2754c02a6e87c5d5c
Lucio Neves <luciocecilia20@gmail.com>
2024-02-15 16:53:47 -0300

    criação de classe controle remoto

--- README.md
+++
@@ -1,2 +0,0 @@
-# eujapecaneves
-Testes
---
+++ minha_classe.py
@@ -0,0 +1,23 @@
+class ControleRemoto:
+
+  def __init__(self, televisao, comodo):
+    self.televisao = televisao
+    self.comodo = comodo
+
+
+  def avancar_canal(self):
+    print('Canal Avancado')  
+
+
+  def voltar_canal(self):
+    print('Voltar o canal')
+
+  def escolher_canal(self, canal):
+    print('Alterado para o canal: {}'.format(canal)) 
+
+controle_sala = ControleRemoto('sansung', 'sala')
+print(controle_sala.comodo)  
+print(controle_sala.televisao)
+controle_sala.avancar_canal()
+controle_sala.escolher_canal(12)   
+
